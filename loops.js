baddca47dd9144e064acbc5fb8719e9c50ddcb26
Raphael <raphael.augusto.almeida@gmail.com>
2023-04-24 21:40:16 -0300

    implement loops comparison

--- loops.js
+++ loops.js
@@ -1,67 +1,63 @@
-const SIZE = 10000000;
+const ARRAY_SIZE = 1000000;
+const SAMPLE_SIZE = 20;
 
-var arr = new Array(SIZE).fill(Math.random());
-function testFor(arr) {
-    console.time('classic-for');
+const results = [];
+const testLoop = (func, name) => {
+    let result = 0;
+    for (let i = 0; i < SAMPLE_SIZE; i++) {
+        var arr = new Array(ARRAY_SIZE).fill(Math.random());
+        const start = Date.now();
+        func(arr);
+        const end = Date.now();
+        result += end - start;
+    }
+    return {
+        name,
+        average: result / SAMPLE_SIZE
+    };
+
+}
+
+const classicFor = (arr) => {
     var res = [];
     for (let i = 0; i < arr.length; i++) {
         res.push(arr[i]);
     }
-    console.timeEnd('classic-for')
-    return res;
 }
-testFor(arr);
+results.push(testLoop(classicFor, 'classic-for'));
 
-var arr = new Array(SIZE).fill(Math.random());
-function testForIn(arr) {
-    console.time('for-in');
+const forIn = (arr) => {
     var res = [];
     for (var i in arr) {
         res.push(arr[i]);
     }
-    console.timeEnd('for-in')
-    return res;
 }
-testForIn(arr);
+results.push(testLoop(forIn, 'for-in'));
 
-var arr = new Array(SIZE).fill(Math.random());
-function testForOf(arr) {
-    console.time('for-of');
+const forOf = (arr) => {
     var res = [];
     for (let i of arr) {
         res.push(i);
     }
-    console.timeEnd('for-of')
-    return res;
 }
-testForOf(arr);
+results.push(testLoop(forOf, 'for-of'));
 
-var arr = new Array(SIZE).fill(Math.random());
-function testForEach(arr) {
-    console.time('forEach');
+const forEach = (arr) => {
     var res = [];
     arr.forEach((value, index) => {
         res.push(value);
     });
-    console.timeEnd('forEach');
-    return res;
 }
-testForEach(arr);
+results.push(testLoop(forEach, 'forEach'));
 
-var arr = new Array(SIZE).fill(Math.random());
-function testMap(arr) {
-    console.time('map');
+const map = (arr) => {
     var res = arr.map(function (x) {
         return x;
     });
-    console.timeEnd('map')
-    return res;
 }
-testMap(arr);
+results.push(testLoop(map, 'map'));
 
-var arr = new Array(SIZE).fill(Math.random());
-function testWhile(arr) {
-    console.time('While');
+const whileLoop = (arr) => {
     var res = [];
     x = arr.length;
     i = 0;
@@ -69,14 +65,11 @@ function testWhile(arr) {
         res.push(arr[i]);
         i++;
     }
-    console.timeEnd('While');
-    return res;
-}
-testWhile(arr);
 
-var arr = new Array(SIZE).fill(Math.random());
-function testDoWhile(arr) {
-    console.time('DoWhile');
+}
+results.push(testLoop(whileLoop, 'while'));
+
+const doWhile = (arr) => {
     var res = [];
     x = arr.length;
     i = 0;
@@ -84,7 +77,19 @@ function testDoWhile(arr) {
         res.push(arr[i]);
         i++;
     } while (i < x - 1);
-    console.timeEnd('DoWhile');
-    return res;
 }
-testDoWhile(arr);
+results.push(testLoop(doWhile, 'do-while'));
+
+results
+    .sort((a, b) => a.average - b.average)
+    .forEach((result) => console.log(`${result.name}: ${result.average}ms`));
+
+
+// Results on MacBook Pro M1 2020, node v16.15.0 :
+// do-while:    7.15ms
+// for-of:      8.15ms
+// while:       8.15ms
+// classic-for: 8.8ms
+// forEach:     17.25ms
+// map:         35.1ms
+// for-in:      95.9ms
